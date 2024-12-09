07d14f4ff7b63854a235cc377ba6debf1e4845e0
JuanPabaz <jpgthf@gmail.com>
2024-12-09 15:35:12 -0500

    feat: add login function

--- RestaurantReviewAngular/src/app/login/login.component.ts
+++ RestaurantReviewAngular/src/app/login/login.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { AuthRequestDTO } from '../models/auth-request-dto.models';
+import { AuthService } from '../auth.service';
+import { AuthResponseDTO } from '../models/auth-response-dto.models';
+import { Role } from '../models/user.models';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  authRequest: AuthRequestDTO = {
+    username: '',
+    password: ''
+  };
+  authResponse: AuthResponseDTO = {
+    accessToken: '',
+    refreshToken: '',
+    role: Role.NONE
+  }
 
+  constructor(private authService: AuthService){
+
+  }
+
+  login(){
+    debugger
+    this.authService.login(this.authRequest)
+    .subscribe({
+      next:(response) => {
+        this.authResponse = response;
+      },
+      error:(error) => {
+        console.log("Error:" + error.error.message)
+      }
+    })
+  }
 }
