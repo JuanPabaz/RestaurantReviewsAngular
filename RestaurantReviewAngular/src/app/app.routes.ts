b06e7a2bb332c519068df93bc28c8eb65b6ca013
JuanPabaz <jpgthf@gmail.com>
2024-12-10 15:03:01 -0500

    feat: add RestaurantListComponent to route

--- RestaurantReviewAngular/src/app/app.routes.ts
+++ RestaurantReviewAngular/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { RestaurantListComponent } from './components/restaurant-list/restaurant-list.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:"register",
         component: RegisterComponent
+    },
+    {
+        path:"",
+        component:RestaurantListComponent
     }
 ];
