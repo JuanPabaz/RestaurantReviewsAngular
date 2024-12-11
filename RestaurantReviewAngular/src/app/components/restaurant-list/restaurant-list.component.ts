523ea81c2d8a06bd5116c630eadbabe7154dbd58
JuanPabaz <jpgthf@gmail.com>
2024-12-11 08:52:34 -0500

    feat: add openModal and closeModal

--- RestaurantReviewAngular/src/app/components/restaurant-list/restaurant-list.component.ts
+++ RestaurantReviewAngular/src/app/components/restaurant-list/restaurant-list.component.ts
@@ -2,33 +2,45 @@ import { Component } from '@angular/core';
 import { RestaurantResponseDTO } from '../../models/restaurant-response-dto.models';
 import { CommonModule } from '@angular/common';
 import { RestaurantService } from '../../services/restaurant.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurant-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './restaurant-list.component.html',
   styleUrl: './restaurant-list.component.css'
 })
 export class RestaurantListComponent {
   restaurantList: RestaurantResponseDTO[] = [];
+  isModalOpen: boolean = false;
+  selectedRestaurant: RestaurantResponseDTO | null = null;
 
-  constructor(private restaurantService: RestaurantService){
-
-  }
+  constructor(private restaurantService: RestaurantService){}
 
   ngOnInit(){
-    this.getAllRestaurants()
+    this.getAllRestaurants();
   }
 
   getAllRestaurants(){
     this.restaurantService.getAllRestaurants()
     .subscribe({
-      next:(response) => {
+      next: (response) => {
         this.restaurantList = response;
       },
-      error:(error) => {
-        console.log("Error:" + error.error.message)
+      error: (error) => {
+        console.log("Error:" + error.error.message);
       }
-    })
+    });
+  }
+
+  openModal(restaurante: RestaurantResponseDTO): void {
+    console.log("modal")
+    this.selectedRestaurant = restaurante;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.selectedRestaurant = null;
   }
 }
