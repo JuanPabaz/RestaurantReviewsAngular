420b8b83fce580a4095c4af63c06e5500c375221
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-05 11:43:47 -0500

    feat: add restaurant list as a child of layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,15 @@ export const routes: Routes = [
             {
                 path:'login',
                 loadComponent: () => import('./business/auth/login/login.component')
+            },
+            {
+                path:'restaurant-list',
+                loadComponent: () => import('./business/restaurant/restaurant-list/restaurant-list.component')
+            },
+            {
+                path: '',
+                redirectTo: 'restaurant-list',
+                pathMatch: 'full'
             }
         ]
     }
