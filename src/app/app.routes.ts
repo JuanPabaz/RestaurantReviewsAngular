c8110140ac1ad57dc79a3ffc04364def76c70b76
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-05 10:36:04 -0500

    feat: add login component as a child of layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path:'',
-        loadComponent: () => import('./shared/components/layout/layout.component')
-    },
-    {
-        path:'login',
-        loadComponent: () => import('./business/auth/login/login.component')
+        loadComponent: () => import('./shared/components/layout/layout.component'),
+        children: [
+            {
+                path:'login',
+                loadComponent: () => import('./business/auth/login/login.component')
+            }
+        ]
     }
 ];
