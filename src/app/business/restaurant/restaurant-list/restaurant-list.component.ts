4d80e08e4430c6ae08f0ed60f15efba2c5e1cc20
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-14 12:44:01 -0500

    feat: call getAllRestaurantes on ngOnInit

--- src/app/business/restaurant/restaurant-list/restaurant-list.component.ts
+++ src/app/business/restaurant/restaurant-list/restaurant-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from '../../../shared/services/restaurant.service';
+import { RestaurantResponse } from '../../../interfaces/restaurant-response';
+import { CarouselComponent } from '../../../shared/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-restaurant-list',
@@ -6,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './restaurant-list.component.html',
   styleUrl: './restaurant-list.component.css'
 })
-export default class RestaurantListComponent {
+export default class RestaurantListComponent implements OnInit{
+  restaurantList: RestaurantResponse[] = [];
+
+  constructor(private _restaurant_service: RestaurantService){
+
+  }
+
+  ngOnInit(): void {
+    this._restaurant_service.getAllRestaurants().subscribe({
+      next: restaurant => {
+        this.restaurantList = restaurant;
+        this.restaurantList.map(rest => {
+          rest.images.map(images => {
+
+          })
+        })
+      },
+      error: err => {
+        console.log(err.error);
+      }
+    })
+  }
+
 
 }
