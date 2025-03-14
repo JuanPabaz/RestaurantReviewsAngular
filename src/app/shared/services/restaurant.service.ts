69f88fbfbf4b24c42cebe402bc8672f13281dacc
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-14 12:43:31 -0500

    feat: create getAllRestaurants endpoint

--- src/app/shared/services/restaurant.service.ts
+++ src/app/shared/services/restaurant.service.ts
@@ -1,11 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RestaurantResponse } from '../../interfaces/restaurant-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
+  private baseUrl = 'http://localhost:8080/api/1.0';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllRestaurants():Observable<RestaurantResponse[]>{
+    return this.http.get<RestaurantResponse[]>(`${this.baseUrl}/restaurant`);
+  }
 
   
 }
