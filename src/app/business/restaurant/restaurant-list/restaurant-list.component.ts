060e36933dc28e527e5d45c35d14d0e425c92f4a
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-14 16:36:34 -0500

    feat: import pageable component and add changePage method

--- src/app/business/restaurant/restaurant-list/restaurant-list.component.ts
+++ src/app/business/restaurant/restaurant-list/restaurant-list.component.ts
@@ -2,29 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { RestaurantService } from '../../../shared/services/restaurant.service';
 import { RestaurantResponse } from '../../../interfaces/restaurant-response';
 import { CarouselComponent } from '../../../shared/components/carousel/carousel.component';
+import { Pageable } from '../../../interfaces/pageable';
+import { PageableComponent } from '../../../shared/components/pageable/pageable.component';
 
 @Component({
   selector: 'app-restaurant-list',
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, PageableComponent],
   templateUrl: './restaurant-list.component.html',
   styleUrl: './restaurant-list.component.css'
 })
 export default class RestaurantListComponent implements OnInit{
   restaurantList: RestaurantResponse[] = [];
+  restaurantListPageable!: Pageable<RestaurantResponse[]>;
 
   constructor(private _restaurant_service: RestaurantService){
 
   }
 
   ngOnInit(): void {
-    this._restaurant_service.getAllRestaurants().subscribe({
+    this._restaurant_service.getAllRestaurants(1).subscribe({
       next: restaurant => {
-        this.restaurantList = restaurant;
-        this.restaurantList.map(rest => {
-          rest.images.map(images => {
+        this.restaurantListPageable = restaurant;
+        this.restaurantList = restaurant.content;
+      },
+      error: err => {
+        console.log(err.error);
+      }
+    })
+  }
 
-          })
-        })
+  changePage(page:number){
+    this._restaurant_service.getAllRestaurants(page).subscribe({
+      next: restaurant => {
+        this.restaurantListPageable = restaurant;
+        this.restaurantList = restaurant.content;
       },
       error: err => {
         console.log(err.error);
