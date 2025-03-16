ce49c543196254a40ef69d93032e40c9bd6df7fa
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-16 17:28:36 -0500

    feat: add selectedRestaurant, validated through the constructor and send restaurant id in createReview

--- src/app/business/create-review/create-review.component.ts
+++ src/app/business/create-review/create-review.component.ts
@@ -3,6 +3,8 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { ReviewsService } from '../../shared/services/reviews.service';
 import { ReviewRequest } from '../../interfaces/review-request';
 import { CommonModule } from '@angular/common';
+import { RestaurantResponse } from '../../interfaces/restaurant-response';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-review',
@@ -12,6 +14,15 @@ import { CommonModule } from '@angular/common';
 })
 export default class CreateReviewComponent {
   stars = [1, 2, 3, 4, 5];
+  selectedRestaurant: RestaurantResponse = {
+    idRestaurant: 0,
+    restuarantName: '',
+    address: '',
+    phoneNumber: '',
+    idCategory: 0,
+    pageLink: '',
+    images: [],
+  };
   reviewRequest: ReviewRequest = {
     place: -1,
     food: -1,
@@ -25,15 +36,20 @@ export default class CreateReviewComponent {
     idUser: 0,
     images: [],
   }
-  constructor(private review_service: ReviewsService){
-
+  constructor(
+    private review_service: ReviewsService,
+    private router: Router){
+    if (router.getCurrentNavigation()?.extras.state){
+      this.selectedRestaurant = router.getCurrentNavigation()?.extras.state!['restaurant'];
+    }
   }
 
   onSubmit(reviewForm:NgForm){
     debugger
     let reviewRequest = reviewForm.form.value;
     const userId = JSON.parse(sessionStorage.getItem('user')!).userId;
-    reviewRequest = {...reviewRequest, idUser: userId}; 
+    const restaurantId = this.selectedRestaurant.idRestaurant;
+    reviewRequest = {...reviewRequest, idUser: userId, restaurantId}; 
     this.review_service.createReview(reviewRequest).subscribe({
       next: reviewResponse => {
         console.log(reviewResponse);
