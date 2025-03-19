bb8a3691e11e7daa55d1d813b8e5c7ce40a77d32
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-19 12:29:52 -0500

    feat: initialize componente with the review list

--- src/app/business/review/review-list/review-list.component.ts
+++ src/app/business/review/review-list/review-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReviewResponse } from '../../../interfaces/review-response';
+import { ReviewsService } from '../../../shared/services/reviews.service';
+import { Pageable } from '../../../interfaces/pageable';
 
 @Component({
   selector: 'app-review-list',
@@ -6,6 +9,29 @@ import { Component } from '@angular/core';
   templateUrl: './review-list.component.html',
   styleUrl: './review-list.component.css'
 })
-export class ReviewListComponent {
+export default class ReviewListComponent implements OnInit{
+  reviewPageable!: Pageable<ReviewResponse[]>;
+  reviewList: ReviewResponse[] = [];
+
+  constructor(private review_service: ReviewsService){
+
+  }
+
+  ngOnInit(): void {
+    this.getAllReviews(1);
+  }
+
+  getAllReviews(page: number){
+    this.review_service.getAllRestaurants(page).subscribe({
+      next: reviews => {
+        this.reviewPageable = reviews;
+        this.reviewList = reviews.content;
+      },
+      error: err => {
+        console.log(err.error);
+      }
+    })
+  }
+
 
 }
