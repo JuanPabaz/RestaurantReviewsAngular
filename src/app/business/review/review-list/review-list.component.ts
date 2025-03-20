316e86b308f4b8e783fc2836d7c5c4e4cc81b62d
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 15:54:36 -0500

    feat: implement filterReviews

--- src/app/business/review/review-list/review-list.component.ts
+++ src/app/business/review/review-list/review-list.component.ts
@@ -4,14 +4,16 @@ import { ReviewsService } from '../../../shared/services/reviews.service';
 import { Pageable } from '../../../interfaces/pageable';
 import { CarouselComponent } from '../../../shared/components/carousel/carousel.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-review-list',
-  imports: [CarouselComponent, CommonModule],
+  imports: [CarouselComponent, CommonModule, FormsModule],
   templateUrl: './review-list.component.html',
   styleUrl: './review-list.component.css'
 })
 export default class ReviewListComponent implements OnInit{
+  searchItem:string = '';
   dots = [1,2,3];
   reviewPageable!: Pageable<ReviewResponse[]>;
   reviewList: ReviewResponse[] = [];
@@ -34,6 +36,23 @@ export default class ReviewListComponent implements OnInit{
         console.log(err.error);
       }
     })
+  }
+
+  filterReview() {
+    if (this.searchItem.trim()) {
+      this.review_service.filterReviews(1, this.searchItem).subscribe({
+        next: reviews => {
+          this.reviewPageable = reviews;
+          this.reviewList = reviews.content;
+          console.log(this.reviewList);
+        },
+        error: err => {
+          console.log(err.error);
+        }
+      });
+    } else {
+      this.getAllReviews(1);
+    }
   }
 
   getStars(score: number): boolean[] {
