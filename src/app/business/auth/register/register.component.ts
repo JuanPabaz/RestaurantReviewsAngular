60afc19af1aac7f1efab1bd945e686914a840233
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-25 10:45:37 -0500

    feat: add request and constructor attributes

--- src/app/business/auth/register/register.component.ts
+++ src/app/business/auth/register/register.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../shared/services/auth.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RegisterRequest } from '../../../interfaces/register-request';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export default class RegisterComponent {
+  registerRequest: RegisterRequest = {
+    username: '',
+    password: '',
+    fullName: '',
+    role: ''
+  }
 
+  constructor(
+    private auth_service: AuthService,
+    private router: Router){
+
+  }
+
+  onSubmit(registerForm: NgForm){
+
+  }
 }
