f5e4192fe6ecdbc545070f4b1a30e735047b6e98
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-27 12:15:04 -0500

    feat: get restaurant through state

--- src/app/business/restaurant/restaurant-detail/restaurant-detail.component.ts
+++ src/app/business/restaurant/restaurant-detail/restaurant-detail.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { RestaurantResponse } from '../../../interfaces/restaurant-response';
+import { Router } from '@angular/router';
+import { CarouselComponent } from '../../../shared/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-restaurant-detail',
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './restaurant-detail.component.html',
   styleUrl: './restaurant-detail.component.css'
 })
-export class RestaurantDetailComponent {
+export default class RestaurantDetailComponent {
+  selectedRestaurant: RestaurantResponse = {
+    idRestaurant: 0,
+    restuarantName: '',
+    address: '',
+    phoneNumber: '',
+    idCategory: 0,
+    pageLink: '',
+    images: [],
+  };
 
+  constructor(private router: Router){
+    if (router.getCurrentNavigation()?.extras.state){
+      this.selectedRestaurant = router.getCurrentNavigation()?.extras.state!['restaurant'];
+    }
+  }
 }
