526950b2f940c391937039fcc6c8ece049167be7
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-04 15:38:40 -0500

    feat: create profile layout component

---
+++ src/app/business/profile/profile-layout/profile-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-layout',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './profile-layout.component.html',
+  styleUrl: './profile-layout.component.css'
+})
+export default class ProfileLayoutComponent {
+
+}
