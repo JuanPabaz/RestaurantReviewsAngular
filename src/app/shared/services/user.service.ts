0341142934a3ec8df6c73ec20ee63d77c2f8b27f
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-05 11:30:27 -0500

    feat: add getUserById endpoint

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FavoriteResponse } from '../../interfaces/favorite-response';
+import { UserResponse } from '../../interfaces/user-response';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class UserService {
 
   addToFavorites(idRestaurant: number, idUser: number):Observable<FavoriteResponse>{
     return this.http.post<FavoriteResponse>(`${this.baseUrl}/favorites?idRestaurant=${idRestaurant}&idUser=${idUser}`, null);
+  }
+
+  getUserById(idUser: number):Observable<UserResponse>{
+    return this.http.get<UserResponse>(`${this.baseUrl}/${idUser}`);
   }
 }
