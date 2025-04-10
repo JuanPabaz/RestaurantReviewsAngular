9e74d67f16d06f98d7f309cfa2f44989a762fb16
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-10 10:22:14 -0500

    feat: add event listener to close the submenu if it is clicked outside of it

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -9,7 +9,8 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  
+  @ViewChild('subMenu') subMenu!: ElementRef;
+  @ViewChild('userPic') userPic!: ElementRef;
 
   constructor(
     private auth_service: AuthService,
@@ -23,6 +24,21 @@ export class NavbarComponent {
   toggleProfileMenu(){
     const subMenu = document.getElementById("subMenu");
     subMenu?.classList.toggle("open-menu");
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const subMenuElement = this.subMenu?.nativeElement;
+    const userPicElement = this.userPic?.nativeElement;
+    
+    if (subMenuElement && userPicElement) {
+      const isClickInsideMenu = subMenuElement.contains(event.target);
+      const isClickOnUserPic = userPicElement.contains(event.target);
+      
+      if (!isClickInsideMenu && !isClickOnUserPic && subMenuElement.classList.contains('open-menu')) {
+        subMenuElement.classList.remove('open-menu');
+      }
+    }
   }
 
   logoutHandler(){
